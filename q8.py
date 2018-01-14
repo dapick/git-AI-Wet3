5278b2af8119997315c91d4c691fe814354d79d7
Daniella Pick <danielapick@gmail.com>
2018-01-14 20:59:08 +0200

    Finished question 8

---
+++ q8.py
@@ -0,0 +1,18 @@
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import accuracy_score
+from id3 import Id3Estimator
+from extractData import X, y
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=17)
+
+# Without pruning
+no_prone_accuracy = cross_val_score(Id3Estimator(), X_train, y_train, cv=4, scoring='accuracy').mean()
+print("Accuracy without pruning on cross validation is:", no_prone_accuracy)
+
+# With pruning
+estimator = Id3Estimator(min_samples_split=20)
+no_prone_accuracy = cross_val_score(estimator, X_train, y_train, cv=4, scoring='accuracy').mean()
+print("Accuracy with pruning on cross validation is:", no_prone_accuracy)
+
+estimator.fit(X_train, y_train)
+print("Accuracy with pruning on test is:", accuracy_score(y_test, estimator.predict(X_test)))
